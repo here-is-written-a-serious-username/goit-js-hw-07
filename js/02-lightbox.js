e062dea8896f273c21776d439920b8f33bf8f540
Lord-Bambucha <lordbambycha93@gmail.com>
2023-01-03 00:42:32 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divGallery = document.querySelector(".gallery");
+
+// divGallery.addEventListener("click", onImgClick);
+
+divGallery.insertAdjacentHTML("beforeend", createGalleryMarkup());
+
+
+function createGalleryMarkup() {
+    const markup = galleryItems.map(imgMarkupMaker).join("");
+
+    function imgMarkupMaker({ preview, original, description }) {
+        return (
+            `<a class="gallery__item" href="${original}">
+                <img 
+                class="gallery__image" 
+                src= "${preview}"
+                alt= "${description}"
+                />
+            </a>`
+        );
+    }
+
+    return markup;
+}
+
+new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
